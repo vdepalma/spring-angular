019f824afbb51b97de9e070cdfa76c3582ab02dc
vdepalma <vdepalma@nalandaglobal.com>
2019-09-23 17:52:52 +0200

    agregando cliente form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service'
 import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import { FormComponent } from './clientes/form.component';
+import { FormsModule } from '@angular/forms';
 
 
 const routes: Routes= [
   {path: '',redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'directivas',component: DirectivaComponent},
-  {path: 'clientes',component: ClientesComponent}
+  {path: 'clientes',component: ClientesComponent},
+  {path: 'clientes/form', component: FormComponent}
 ];
 
 @NgModule({
@@ -22,10 +26,13 @@ const routes: Routes= [
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [
